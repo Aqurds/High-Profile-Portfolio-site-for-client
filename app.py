e9206b8ad40a314a6d7a9c5aa02a24e45e775708
Aqurds <omarf1320@gmail.com>
2019-04-04 05:05:25 +0600

    updated

--- app.py
+++ app.py
@@ -13,19 +13,53 @@ def home():
 
 
 
+# About route
+@app.route('/about/')
+def about():
+    return render_template('about.html')
+
+
+
+# Speaking route
+@app.route('/speaking/')
+def speaking():
+    return render_template('speaking.html')
+
+
+
+# Book route
+@app.route('/book/')
+def book():
+    return render_template('book.html')
+
+
+
+# blog route
+@app.route('/blog/')
+def blog():
+    return render_template('blog.html')
+
+
+
+# Single blog route
+@app.route('/single-blog/')
+def single_blog():
+    return render_template('single-blog.html')
+
+
+
+# Contact route
+@app.route('/contact/')
+def contact():
+    return render_template('contact.html')
 
 
 
 
-
-
-
-
-
-
-
-
-
+# Admin route
+@app.route('/admin/')
+def admin():
+    return render_template('admin.html')
 
 
 
