aca16438c28a790d4ded835590ff045b516a4ed8
Aqurds <omarf1320@gmail.com>
2019-04-11 19:32:44 +0600

    almost done

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, url_for, jsonify
+from flask import Flask, render_template, request, redirect, url_for, jsonify, session
 import os
 import smtplib
 import imghdr
@@ -15,6 +15,7 @@ db = pymongo.database.Database(mongo, 'edoblog')
 
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = '0f9dc56d2288afa6e10b8d97577fe25b'
 
 
 
@@ -100,8 +101,20 @@ def post():
     # Fetching the database for all blog post
     col = pymongo.collection.Collection(db, 'blog_post')
     single_post = list(col.find({'title': post_title_as_id}))
+    all_post = list(col.find())
+    index_of_current_post = 0
+    for post in all_post:
+        if post['title'] == post_title_as_id:
+            index_of_current_post = all_post.index(post)
 
-    return render_template('post.html', post_title_as_id=post_title_as_id, single_post=single_post)
+    del all_post[index_of_current_post]
+
+    # Fetching comments from comment collection
+    col = pymongo.collection.Collection(db, 'comments')
+    comments = list(col.find({'title':post_title_as_id}))
+
+
+    return render_template('post.html', post_title_as_id=post_title_as_id, single_post=single_post, all_post=all_post, comments=comments)
 
 
 
@@ -114,9 +127,88 @@ def contact():
 
 
 # Admin route
-@app.route('/admin/')
+@app.route('/admin/', methods=['GET', 'POST'])
 def admin():
-    return render_template('admin.html')
+    if session:
+        file_name = ''
+
+        if request.method == "POST":
+            title = request.form['title']
+            body = request.form['post_body']
+            post_image = request.files['image']
+            post_image_name = post_image.filename
+            picture_path = os.path.join(app.root_path, 'static/img/blogimage', post_image_name)
+            # file.save(os.path.join(image_folder, file_name))
+            post_image.save(picture_path)
+            col = pymongo.collection.Collection(db, 'blog_post')
+            col.insert({'title':title, 'body':body, 'image':post_image_name})
+        return render_template('admin.html')
+    else:
+        return redirect(url_for('login'))
+
+
+
+
+# Login route
+@app.route('/login/', methods=['POST', 'GET'])
+def login():
+    if session:
+        return redirect(url_for('admin'))
+
+    return render_template('login.html')
+
+
+
+
+# Login form AJAX call process route
+@app.route('/loginprocess', methods=['POST'])
+def loginprocess():
+    username = request.form['name']
+    password = request.form['password']
+
+    # Fetching the database for admin authentication
+    col = pymongo.collection.Collection(db, 'jocobs')
+    admin_user = list(col.find({'name' : username}))
+
+    if admin_user:
+        if password == admin_user[0]["password"]:
+            session['username'] = username
+            return redirect(url_for('admin'))
+    else:
+        return jsonify({'error': 'Invalid username or password!'})
+
+
+
+# Login form AJAX call process route
+@app.route('/commentprocess', methods=['POST'])
+def commentprocess():
+    username = request.form['name']
+    email = request.form['email']
+    comment = request.form['comment']
+    title = request.form['title']
+    data = {
+    "name": username,
+    "email": email,
+    "comment": comment
+    }
+
+    # Inserting comment with title reference in comments collection
+    col = pymongo.collection.Collection(db, 'comments')
+    col.insert({"title": title, "comment": data})
+    return jsonify({'feedback': 'Invalid username or password!'})
+
+
+
+
+
+# Logout route
+@app.route('/logout/')
+def logout():
+    if session:
+        session.clear()
+        return redirect(url_for('login'))
+    else:
+        return redirect(url_for('home'))
 
 
 
